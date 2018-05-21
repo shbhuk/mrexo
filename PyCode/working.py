25d5c60dcfbb0d07b122061ccc3514c11b7f9d34
gummiks <gummiks@gmail.com>
2018-05-21 19:09:44 -0400

    change number of cores

--- PyCode/working.py
+++ PyCode/working.py
@@ -148,7 +148,7 @@ def MLE_fit_bootstrap(data, sigma, Mass_max = None, Mass_min = None, Radius_max 
             
             
 if __name__ == '__main__':           
-    a = MLE_fit_bootstrap(data = data, sigma = sigma, Mass_max = Mass_max, Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 3, Log = True, num_boot = 8, cores = 8)
+    a = MLE_fit_bootstrap(data = data, sigma = sigma, Mass_max = Mass_max, Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 3, Log = True, num_boot = 32)
             
             
         
