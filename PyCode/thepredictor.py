6bb92cf345189dd621b7015c612f5901db5fcf04
shbhuk <shbhuk@gmail.com>
2018-08-05 21:37:43 -0400

    predictor

--- PyCode/thepredictor.py
+++ PyCode/thepredictor.py
@@ -46,7 +46,7 @@ def predict_mass_given_radius(radius, r_sigma = None, posterior_sample = False, 
         k = np.size(radius)
         mean_sample = np.zeros(k)
         denominator_sample = np.zeros(k)
-        y_beta_indv_sampple = np.zeros((k,degrees))
+        y_beta_indv_sample = np.zeros((k,degrees))
         
         # the model can be view as a mixture of k conditional densities for f(log m |log r), each has weight 1/k
         # the mean of this mixture density is 1/k times sum of the mean of each conditional density
@@ -58,6 +58,73 @@ def predict_mass_given_radius(radius, r_sigma = None, posterior_sample = False, 
             results = MLE_fit.cond_density_quantile(y = logRadius[i], y_std = r_sigma[i], y_max = Radius_max, y_min = Radius_min,
                                                       x_max = Mass_max, x_min = Mass_min, deg = degrees, 
                                                       w_hat = weights_mle, qtl = qtl)
+                                                      
+            mean_sample[i] = results[0]
+            denominator_sample[i] = results[4]
+            y_beta_indv_sample[i,:] = results[5:]
+        
+        predicted_mean = np.mean(mean_sample)
+        
+        def pbeta_conditional_density(x):
+            
+            def mix_density(j):
+                
+                x_indv_cdf = np.array([beta.cdf((j - x_min)/(x_max - x_min), a = d, b = deg - d + 1) for d in deg_vec])
+    
+                quantile_nominator = np.sum(w_hat * np.kron(x_indv_cdf,y_beta_indv))
+                p_beta = quantile_nominator / denominator
+                
+                return p_beta
+    
+            if np.size(x)>1:
+                return np.array([mix_density(i) for i in x])
+            else:
+                return mix_density(x)
+            
+            
+        def conditional_quantile(q):
+            def g(x):
+                return pbeta_conditional_density(x) - q
+            return root(g,a = x_min, b = x_max)
+ 
+        quantile = [conditional_quantile(i) for i in qtl]
+        
+        
+        
+        
+        
+            pbeta.conditional.density <- function(x){ 
+      
+      mix.density <- function(j) {
+        
+        deg.vec <- 1:55
+        x.indv.cdf <-
+          sapply(deg.vec, 
+                 function(x, degree) {pbeta(x, degree, deg-degree+1)}, 
+                 x = (j-x.min)/(x.max-x.min))
+        quantile.numerator <- rep(0,k)
+        p.beta.sample <- rep(0, k)
+        for (ii in 1:k) {
+          quantile.numerator[ii] <- 
+            sum(weights.mle * kronecker(x.indv.cdf, y.beta.indv.sample[ii, ]))
+          p.beta.sample[ii] <- quantile.numerator[ii]/denominator.sample[ii]
+        }
+        p.beta <- mean(p.beta.sample)
+      }
+      sapply(x, mix.density)
+    }
+  
+    mixture.conditional.quantile <- function(q, x.min, x.max){ 
+      g <- function(x){ pbeta.conditional.density(x)-q }
+      root <- uniroot(g, interval=c(x.min, x.max) )$root
+      return(root) 
+    }
+    predicted.quantiles <- sapply(qtl, mixture.conditional.quantile,
+                                  x.min = Mass.min, x.max = Mass.max)
+    predicted.lower.quantile <- predicted.quantiles[1]
+    predicted.upper.quantile <- predicted.quantiles[2]
+    
+  } 
         
         
     return predicted_mean,predicted_lower_quantile,predicted_upper_quantile
