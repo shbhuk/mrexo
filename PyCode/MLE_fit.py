3e07457175dceabb194bc35697686d11636750c8
shbhuk <shbhuk@gmail.com>
2018-08-08 20:51:04 -0400

    optimization restuls

--- PyCode/MLE_fit.py
+++ PyCode/MLE_fit.py
@@ -293,11 +293,11 @@ def MLE_fit(data, bounds, deg, sigma = None, Log = False,
     #print('Using slsqp with bigger steps')
     #opt_result = minimize(fun = fn2, x0 = x0, bounds = bounds, method = 'Nelder_Mead')
     #opt_result = fmin_l_bfgs_b(fn2, x0, bounds = bounds, iprint = 0, approx_grad = True)
-    opt_result,fx,its,imode,smode = fmin_slsqp(fn2, x0, bounds = bounds, full_output = True, iprint = 1,full_output = True)
+    opt_result = fmin_slsqp(fn2, x0, bounds = bounds, full_output = True, iprint = 1)
     #opt_result = fmin_slsqp(fn1, x0, bounds = bounds, f_eqcons = eqn, iter = 1e3,full_output = True, iprint = 0)
     print('Optimization run finished at', datetime.datetime.now())
 
-    print('Optimization terminated after {} iterations. Exit Code = {}{}\n\n'.format(its,imode,smode))
+    print('Optimization terminated after {} iterations. Exit Code = {}{}\n\n'.format(opt_result[2],opt_result[3],opt_result[4]))
     
     w_hat = opt_result[0]
     n_log_lik = opt_result[1]
