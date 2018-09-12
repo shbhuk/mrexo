64b032ed6d8ef0bc18281fddf036479bd21cafb3
Shubham <shbhuk@gmail.com>
2018-09-12 17:49:48 -0400

    pbs script

--- PyCode/working.py
+++ PyCode/working.py
@@ -238,8 +238,8 @@ def MLE_fit_bootstrap(data, sigma, Mass_max = None, Mass_min = None, Radius_max 
             
 if __name__ == '__main__':           
     a = MLE_fit_bootstrap(data = data, sigma = sigma, Mass_max = Mass_max, 
-                        Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 55, Log = True, num_boot = 1, cores = 1,
-                        location = os.path.join(os.path.dirname(__file__),'Apple_fullworking_core1'))
+                        Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 55, Log = True, num_boot = 100, cores = 20,
+                        location = os.path.join(os.path.dirname(__file__),'Bootstrap_cyberlamp'))
             
             
         
