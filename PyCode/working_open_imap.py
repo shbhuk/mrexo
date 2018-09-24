53a0e8a126a94dbc52e511cd2ab8136b533a6d09
shbhuk <szk381@aci-lgn-003.acib.production.int.aci.ics.psu.edu>
2018-09-23 21:03:09 -0400

    Cluster list imap

--- PyCode/working_open_imap.py
+++ PyCode/working_open_imap.py
@@ -244,5 +244,5 @@ def MLE_fit_bootstrap(data, sigma, Mass_max = None, Mass_min = None, Radius_max 
             
 if __name__ == '__main__':           
     a = MLE_fit_bootstrap(data = data, sigma = sigma, Mass_max = Mass_max, 
-                        Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 55, Log = True, num_boot = 20, 
-                        location = os.path.join(os.path.dirname(__file__),'Bootstrap_cyberlamp_imap2'))
+                        Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 55, Log = True, num_boot = 2, 
+                        location = os.path.join(os.path.dirname(__file__),'Bootstrap_open_imap333'))
