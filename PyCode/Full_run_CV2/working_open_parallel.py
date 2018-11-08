bd332b5bf3bed79a51ffa2d9d40aa9513bc8fd99
shbhuk <szk381@aci-lgn-002.acib.production.int.aci.ics.psu.edu>
2018-11-08 09:47:01 -0500

    Cluster

--- PyCode/Full_run_CV2/working_open_parallel.py
+++ PyCode/Full_run_CV2/working_open_parallel.py
@@ -344,4 +344,4 @@ def MLE_fit_bootstrap(Mass, Radius, Mass_sigma, Radius_sigma, Mass_max = None, M
 if __name__ == '__main__':
     a = MLE_fit_bootstrap(Mass = M_obs, Radius = R_obs, Mass_sigma = M_sigma, Radius_sigma = R_sigma, Mass_max = Mass_max,
                         Mass_min = Mass_min, Radius_max = Radius_max, Radius_min = Radius_min, select_deg = 'cv', Log = True, num_boot = 100,
-                        location = os.path.join(os.path.dirname(__file__),'Full_run_CV3'))
+                        location = os.path.join(os.path.dirname(__file__),'Full_run_CV2'))
