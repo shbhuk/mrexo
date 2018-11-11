5074b9ad872835f4b52dad8616b184b671c5b770
Shubham <shbhuk@gmail.com>
2018-11-11 13:13:06 -0500

    Mdwarf runs

--- PyCode/plot.py
+++ PyCode/plot.py
@@ -14,7 +14,7 @@ print(pwd)
 result_dir = os.path.join(pwd,'Bootstrap_results_cluster50')
 result_dir = os.path.join(pwd,'Bootstrap_cyberlamp_parallel_full2')
 result_dir = os.path.join(pwd,'Results','M_dwarfs_logtrue')
-result_dir = os.path.join(pwd,'M_dwarfs_degree_22')
+result_dir = os.path.join(pwd,'M_dwarfs_degree_29')
 #result_dir = os.path.join(pwd,'test')
 
 t = Table.read(os.path.join(pwd,'MR_Kepler_170605_noanalytTTV_noupplim.csv'))
