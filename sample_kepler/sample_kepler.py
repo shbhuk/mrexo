c44017a2aeab1e5d9cb68f18620a76a45a2f0d62
Shubham <shbhuk@gmail.com>
2018-12-01 10:19:34 -0500

    Restart kepler

--- sample_kepler/sample_kepler.py
+++ sample_kepler/sample_kepler.py
@@ -21,8 +21,8 @@ Radius = np.array(t['pl_rade'])
 # Directory to store results in 
 result_dir = os.path.join(pwd,'Kepler_55_run')
 
-
-initialfit_result, bootstrap_results = fit_mr_relation(Mass = Mass, Mass_sigma = Mass_sigma,
-                                        Radius = Radius, Radius_sigma = Radius_sigma,
-                                        save_path = result_dir, select_deg = 55, 
-                                        num_boot = 20, cores = 20)
+if __name__ == '__main__':
+    initialfit_result, bootstrap_results = fit_mr_relation(Mass = Mass, Mass_sigma = Mass_sigma,
+                                            Radius = Radius, Radius_sigma = Radius_sigma,
+                                            save_path = result_dir, select_deg = 55, 
+                                            num_boot = 20, cores = 20)
