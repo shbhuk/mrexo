ef989761a00dc27c75c3a2e9bf0e6e4bee0b8efd
shbhuk <shbhuk@gmail.com>
2018-12-12 23:33:36 -0500

    kepler cluster script

--- sample_kepler2/sample_kepler.py
+++ sample_kepler2/sample_kepler.py
@@ -14,26 +14,20 @@ t = Table.read(os.path.join(pwd,'MR_Kepler_170605_noanalytTTV_noupplim.csv'))
 Mass_sigma = (abs(t['pl_masseerr1']) + abs(t['pl_masseerr2']))/2 * 0
 Radius_sigma = (abs(t['pl_radeerr1']) + abs(t['pl_radeerr2']))/2 * 0
 
-Mass_sigma = np.repeat(None, len(Mass_sigma))
-Radius_sigma = np.repeat(None, len(Mass_sigma))
+#Mass_sigma = np.repeat(None, len(Mass_sigma))
+#Radius_sigma = np.repeat(None, len(Mass_sigma))
 
 
 # In Earth units
 Mass = np.array(t['pl_masse'])
 Radius = np.array(t['pl_rade'])
 
-Mass_min = -1
-Mass_max = 3.80957
-Radius_min = -0.3
-Radius_max = 1.357509 
 
-# Directory to store results in 
-result_dir = os.path.join(pwd,'Kepler_no_error')
+# Directory to store results in
+result_dir = os.path.join(pwd,'Kepler_55_increased_bounds')
 
 if __name__ == '__main__':
     initialfit_result, bootstrap_results = fit_mr_relation(Mass=Mass, Mass_sigma=Mass_sigma,
                                             Radius=Radius, Radius_sigma=Radius_sigma,
-                                            Mass_min=Mass_min, Mass_max=Mass_max,
-                                            Radius_min=Radius_min, Radius_max=Radius_max,
-                                            save_path=result_dir, select_deg=55, 
+                                            save_path=result_dir, select_deg=55,
                                             num_boot=50, cores=cpu_count())
