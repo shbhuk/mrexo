b0b5c0de36de7a215f66fce60eeb812b64fe848c
Shubham <shbhuk@gmail.com>
2018-12-13 10:02:40 -0500

    Change kepler bounds

--- sample_kepler2/sample_kepler.py
+++ sample_kepler2/sample_kepler.py
@@ -11,23 +11,25 @@ pwd = os.path.dirname(__file__)
 t = Table.read(os.path.join(pwd,'MR_Kepler_170605_noanalytTTV_noupplim.csv'))
 
 # Symmetrical errorbars
-Mass_sigma = (abs(t['pl_masseerr1']) + abs(t['pl_masseerr2']))/2 * 0
-Radius_sigma = (abs(t['pl_radeerr1']) + abs(t['pl_radeerr2']))/2 * 0
+Mass_sigma = (abs(t['pl_masseerr1']) + abs(t['pl_masseerr2']))/2 
+Radius_sigma = (abs(t['pl_radeerr1']) + abs(t['pl_radeerr2']))/2 
 
-#Mass_sigma = np.repeat(None, len(Mass_sigma))
-#Radius_sigma = np.repeat(None, len(Mass_sigma))
 
 
 # In Earth units
 Mass = np.array(t['pl_masse'])
 Radius = np.array(t['pl_rade'])
 
+Mass_min = -1
+Mass_max = 3.80957
+Radius_min = -0.3
+Radius_max = 1.357509 
 
-# Directory to store results in
-result_dir = os.path.join(pwd,'Kepler_55_increased_bounds')
+# Directory to store results in 
+result_dir = os.path.join(pwd,'Kepler_larger_bounds')
 
 if __name__ == '__main__':
     initialfit_result, bootstrap_results = fit_mr_relation(Mass=Mass, Mass_sigma=Mass_sigma,
                                             Radius=Radius, Radius_sigma=Radius_sigma,
-                                            save_path=result_dir, select_deg=55,
+                                            save_path=result_dir, select_deg=55, 
                                             num_boot=50, cores=cpu_count())
