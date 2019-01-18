62347299e561ae9822f9cec769dd7f30be3787b3
shbhuk <szk381@comp-ic-0014.aci.ics.psu.edu>
2019-01-18 13:43:08 -0500

    Cluster

--- sample_scripts/sample_fit.py
+++ sample_scripts/sample_fit.py
@@ -29,5 +29,5 @@ result_dir = os.path.join(pwd,'M_dwarfs_cv')
 if __name__ == '__main__': 
             initialfit_result, bootstrap_results = fit_mr_relation(Mass = Mass, Mass_sigma = Mass_sigma,
                                                 Radius = Radius, Radius_sigma = Radius_sigma,
-                                                save_path = os.path.join(pwd,'M_dwarfs_deg17_final'), select_deg = 17,
-                                                num_boot = 100, cores = cpu_count()-2)
+                                                save_path = os.path.join(pwd,'M_dwarfs_deg17_final2'), select_deg = 'cv',
+                                                num_boot = 100, cores = cpu_count())
