7c0a39deca1cf97a5eaedb7cf93dee23e354cc71
shbhuk <szk381@comp-ic-0014.aci.ics.psu.edu>
2019-01-23 14:19:05 -0500

    cluster lookup

---
+++ sample_scripts/gen_lookup_kepler.py
@@ -0,0 +1,4 @@
+from mrexo.predict import generate_lookup_table
+result_dir = '/storage/home/s/szk381/work/mrexo/mrexo/datasets/Kepler_Ning_etal_20170605'
+
+generate_lookup_table(result_dir = result_dir)
