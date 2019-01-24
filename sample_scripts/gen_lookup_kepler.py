86408ad0effdc369fa1d8e136fd265ccb7d077c7
shbhuk@gmail.com <shbhuk@gmail.com>
2019-01-24 13:21:53 -0500

    Replace mean with median for M dwarf

--- sample_scripts/gen_lookup_kepler.py
+++ sample_scripts/gen_lookup_kepler.py
@@ -2,4 +2,4 @@ from mrexo.predict import generate_lookup_table
 result_dir = '/storage/home/s/szk381/work/mrexo/mrexo/datasets/Kepler_Ning_etal_20170605'
 kepler_result = r'C:\Users\shbhu\Documents\GitHub\mrexo\mrexo\datasets\Kepler_Ning_etal_20170605'
 
-generate_lookup_table(result_dir = kepler_result)
+generate_lookup_table(result_dir = kepler_result, predict_quantity = 'Radius')
