47e8d536f4498a150feeea366f38ee232db78e7c
shbhuk <shbhuk@gmail.com>
2019-02-14 16:20:13 -0500

    gen lookup table m dwarf

--- sample_scripts/sample_predict.py
+++ sample_scripts/sample_predict.py
@@ -1,4 +1,4 @@
-from mrexo import predict_from_measurement
+from mrexo import predict_from_measurement, generate_lookup_table
 import os
 import numpy as np
 
@@ -15,11 +15,16 @@ Sample script to show how to use the predicting function to predict mass from ra
 #Below example predicts the mass for a radius of 1 Earth radii exoplanet, with no measurement uncertainty from the fit results in 'M_dwarfs_dummy'
 result_dir = os.path.join(pwd,'M_dwarfs_pdf_optim')
 result_dir = os.path.join(pwd,'M_dwarfs_new_17')
+
+
+generate_lookup_table(result_dir = result_dir, predict = 'mass')
+generate_lookup_table(result_dir = result_dir, predict = 'radius')
+"""
 predicted_mass, qtls = predict_from_measurement(measurement=1, measurement_sigma=None, result_dir=result_dir, is_posterior=False, show_plot = False)
 
 print(predicted_mass, qtls)
 
-"""
+
 #Below example predicts the mass for a radius of 1 Earth radii exoplanet with uncertainty of 0.1 Earth Radii on the included Mdwarf fit.
 #Similary for Kepler dataset. Also outputs 16,84% qtl
 predicted_mass, qtls = predict_from_measurement(measurement=1, measurement_sigma=0.1, result_dir=None, dataset='mdwarf', is_posterior=False, show_plot=False)
