b628419893bf2657c2ca5ebf207a852e2f2a05ec
shbhuk <szk381@aci-lgn-008.acib.production.int.aci.ics.psu.edu>
2019-02-17 23:46:06 -0500

    kepler interp

--- sample_scripts/sample_predict.py
+++ sample_scripts/sample_predict.py
@@ -85,8 +85,8 @@ if __name__ == '__main__':
     #generate_lookup_table(result_dir = result_dir, predict_quantity = 'radius', cores = 24)
     result_dir = os.path.join(pwd,'Kepler_55_new_pdf')
     a=1
-    generate_lookup_table(result_dir = result_dir, predict= 'mass', cores = 24)
-    generate_lookup_table(result_dir = result_dir, predict_quantity = 'radius', cores = 24)
+    generate_lookup_table(result_dir = result_dir, predict = 'mass', cores = 24)
+    generate_lookup_table(result_dir = result_dir, predict = 'radius', cores = 24)
 
     print(predict_from_measurement(measurement=1, measurement_sigma=None, result_dir=result_dir, use_lookup = True))
 
