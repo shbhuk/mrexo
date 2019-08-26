7733564f051e5b0f61d8db097ccd6b36039709c9
shbhuk <shbhuk@gmail.com>
2019-08-25 21:02:51 -0400

    Remove functools requirement.

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def readme():
 
 
 setup(name='mrexo',
-      version='0.1.4.2.beta',
+      version='0.1.4.2',
       description='Nonparametric mass radius relationship for exoplanets',
       long_description=readme(),
       long_description_content_type="text/markdown",
       url='https://github.com/shbhuk/mrexo',
       author='Shubham Kanodia',
       author_email='shbhuk@gmail.com',
-      install_requires=['astropy>2','matplotlib','numpy','scipy', 'functools32'],
+      install_requires=['astropy>2','matplotlib','numpy','scipy'],
       packages=['mrexo'],
       include_package_data = True,
       license='GPLv3',
       classifiers=['Topic :: Scientific/Engineering :: Astronomy'],
-      keywords='Mass-Radius relationship Non parametric Exoplanets'      )
+      keywords='Mass-Radius relationship Non parametric Exoplanets' )
