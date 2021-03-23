Fix conditional distribution calculation by changing transpose for joint dist

mrexo/mle_utils_nd.py
@@ -424,7 +424,7 @@ def CalculateConditionalDistribution1D_LHS(ConditionString, DataDict,
 	VariancePDF = np.zeros(NPoints)
 	
 	# Initial values
-	ReshapedWeights = np.reshape(weights, deg_per_dim).T
+	ReshapedWeights = np.reshape(weights, deg_per_dim)
 
 	for i in range(NPoints):
 		# Indices = [slice(0, None) for _ in range(DataDict['ndim'])]
@@ -448,7 +448,7 @@ def CalculateConditionalDistribution1D_LHS(ConditionString, DataDict,
 		
 		InterpMesh = np.array(np.meshgrid(*InterpSlices))
 		InterpPoints = np.rollaxis(InterpMesh, 0, ndim+1).reshape((NSeq**(len(LHSTerms)), ndim))
-		SliceofJoint = interpn(tuple(DataDict['DataSequence']), JointDist.T, InterpPoints).reshape(tuple(np.repeat(NSeq, len(LHSTerms))))
+		SliceofJoint = interpn(tuple(DataDict['DataSequence']), JointDist, InterpPoints).reshape(tuple(np.repeat(NSeq, len(LHSTerms))))
 		
 		# Hardcoded 20201209
 		# Take a slice of the joitn distribution (in reality would perhaps need to interpolate this
@@ -493,7 +493,12 @@ def CalculateConditionalDistribution1D_LHS(ConditionString, DataDict,
 		# Variance = E[x^2] - E[x]^2
 		VariancePDF[i] = (UnivariateSpline(LHSSequence[0], ConditionalDist[i]*(LHSSequence[0]**2)).integral(
 			DataDict["ndim_bounds"][LHSDimensions[0]][0], DataDict["ndim_bounds"][LHSDimensions[0]][1])  /  ConditionPDF) - (MeanPDF[i]**2)
-			
+
+		# from scipy.integrate import simps
+		# print(simps(SliceofJoint, LHSSequence[0]))
+		# print(np.sum(temp_denominator))
+		# print(ConditionPDF)
+		
 	return ConditionalDist, MeanPDF, VariancePDF
 
 
@@ -549,7 +554,7 @@ def CalculateConditionalDistribution2D_LHS(ConditionString, DataDict,
 	VariancePDF = np.zeros((NPoints, len(LHSTerms)))
 	
 	# Initial values
-	ReshapedWeights = np.reshape(weights, deg_per_dim).T
+	ReshapedWeights = np.reshape(weights, deg_per_dim)
 
 	for i in range(NPoints):
 		# Indices = [slice(0, None) for _ in range(DataDict['ndim'])]
@@ -581,7 +586,7 @@ def CalculateConditionalDistribution2D_LHS(ConditionString, DataDict,
 		
 		InterpMesh = np.array(np.meshgrid(*InterpSlices))
 		InterpPoints = np.rollaxis(InterpMesh, 0, ndim+1).reshape((NSeq**(len(LHSTerms)), ndim))
-		SliceofJoint = interpn(tuple(DataDict['DataSequence']), JointDist, InterpPoints).reshape(tuple(np.repeat(NSeq, len(LHSTerms))))
+		SliceofJoint = interpn(tuple(DataDict['DataSequence']), JointDist.T, InterpPoints).reshape(tuple(np.repeat(NSeq, len(LHSTerms))))
 		
 		# Hardcoded 20201209
 		# Then calculate denominator by taking matrix multiplication

sample_scripts/3D_marginalize1Dplot.py
@@ -6,7 +6,7 @@ import imageio
 
 from mrexo.mle_utils_nd import calculate_conditional_distribution
 
-ConditionString = 'r,m|stm'
+ConditionString = 'r,p|stm'
 
 ################ Run Conditional Distribution ################ 
 from mrexo.mle_utils_nd import calculate_conditional_distribution
@@ -49,7 +49,7 @@ MeasurementDict = {RHSTerms[0]:[[10**0.0], [np.nan]]}
 # MeasurementDict = {'r':[[1], [np.nan]]}
 
 
-for k in np.arange(0, len(zseq), 1, dtype=int):
+for k in np.arange(0, len(zseq), 2, dtype=int):
 	
 	ChosenZ = zseq[k]
 	print(10**ChosenZ)
@@ -99,7 +99,7 @@ for k in np.arange(0, len(zseq), 1, dtype=int):
 	
 	
 ###############
-
+"""
 ListofPlots = glob.glob(os.path.join(PlotFolder, '3*.png'))
 ListofPlots.sort(key=os.path.getmtime)
 
@@ -109,3 +109,4 @@ for im in ListofPlots:
 		#print(order, im)
 		writer.append_data(imageio.imread(os.path.join(PlotFolder, im)))
 writer.close()
+"""

sample_scripts/sample_fit.py
@@ -131,7 +131,9 @@ InputDictionaries = [RadiusDict, MassDict, StellarMassDict]
 # InputDictionaries = [RadiusDict, StellarMassDict, PeriodDict, MetallicityDict]
 # InputDictionaries = [RadiusDict, StellarMassDict, PeriodDict]
 InputDictionaries = [RadiusDict, MassDict, ]
-# InputDictionaries = [RadiusDict, PeriodDict, StellarMassDict]
+InputDictionaries = [RadiusDict, MassDict,  FakePeriodDict]
+
+
 # InputDictionaries = [RadiusDict, PeriodDict, StellarMassDict]
 # InputDictionaries = [RadiusDict, PeriodDict, MetallicityDict]
 
@@ -143,7 +145,7 @@ save_path = os.path.join(pwd, 'Trial_nd_3Re_incULimits')
 ndim = len(InputDictionaries)
 deg_per_dim = [25, 25, 25, 30]
 deg_per_dim = [35] * ndim
-deg_per_dim = [35, 30]
+# deg_per_dim = [35, 30, 32]
 """
 outputs = MLE_fit(DataDict, 
 	deg_per_dim=deg_per_dim,
@@ -251,8 +253,8 @@ ConditionString = 'm|r'
 # ConditionString = 'm|r,stm'
 # ConditionString = 'm,r|p,stm'
 # ConditionString = 'm,r|feh'
-# ConditionString = 'm,r|p'
-# ConditionString = 'r|stm'
+ConditionString = 'm,r|p'
+# ConditionString = 'm,r|stm'
 
 
 DataDict = DataDict
@@ -263,17 +265,19 @@ MeasurementDict = {'stm':[[0.2, 0.4, 0.43, 0.46, 0.49, 0.52, 0.55, 0.57, 0.6], [
 MeasurementDict = {'feh':[[10**0.0], [np.nan]]}
 MeasurementDict = {'r':[[1], [np.nan]]}
 
+# ConditionString = 'r,p|stm'
+MeasurementDict = {'stm':[[0.5], [np.nan]]}
+MeasurementDict = {'p':[[1], [np.nan]]}
+
+
+
 # MeasurementDict = {'stm':[[0.2], [np.nan]]}
 LogMeasurementDict = {k:np.log10(MeasurementDict[k]) for k in MeasurementDict.keys()}
 
 ConditionalDist, MeanPDF, VariancePDF = calculate_conditional_distribution(ConditionString, DataDict, weights, deg_per_dim,
-	JointDist.T, LogMeasurementDict)
+	JointDist, LogMeasurementDict)
 
-x = DataDict['DataSequence'][0]
-
-plt.figure()
-plt.plot(x, ConditionalDist[0], label=MeasurementDict['r'][0])
-plt.show(block=False)
+x = DataDict['DataSequence'][1]
 
 from scipy.integrate import simps
 from scipy.interpolate import interpn
@@ -313,9 +317,9 @@ plt.tight_layout()
 
 XTicks = np.linspace(xseq.min(), xseq.max(), 5)
 # XTicks = np.log10(np.array([0.3, 1, 3, 10, 30, 100, 300]))
-XTicks = np.log10(np.array([1, 3, 5, 10]))
+# XTicks = np.log10(np.array([1, 3, 5, 10]))
 YTicks = np.linspace(yseq.min(), yseq.max(), 5)
-YTicks = np.log10(np.array([1, 10, 30, 100, 300]))
+# YTicks = np.log10(np.array([1, 10, 30, 100, 300]))
 
 XLabels = np.round(10**XTicks, 1)
 YLabels = np.round(10**YTicks, 2)